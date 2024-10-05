0d3f4e6b6d521c0b79a6650d94a0b36bc657aa37
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-10-05 12:22:56 +0300

    removed unused code

--- Server/src/Infrastructure/Repositories/UsersRepository.cs
+++ Server/src/Infrastructure/Repositories/UsersRepository.cs
@@ -7,9 +7,6 @@ namespace Infrastructure.Repositories;
 
 public class UsersRepository(CineSlateContext dbContext) : IUsersRepository
 {
-    // private readonly List<User> _users = [
-    //     new() { Email = "JohnDoe@test.com", FirstName = "John", LastName = "Doe", Password = "Johns3cret!" },
-    //     new() { Email = "JaneDane@test.com", FirstName = "Jane", LastName = "Dane", Password = "Janes3cret!" }];
     private readonly CineSlateContext _dbContext = dbContext;
 
     public async Task AddUserAsync(User user)
