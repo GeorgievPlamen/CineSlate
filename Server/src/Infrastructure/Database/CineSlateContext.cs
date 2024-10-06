407b0660d1c61092e38a6481df98198bf91a4da0
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-10-06 12:12:52 +0300

    Added model configuration

--- Server/src/Infrastructure/Database/CineSlateContext.cs
+++ Server/src/Infrastructure/Database/CineSlateContext.cs
@@ -6,4 +6,11 @@ namespace Infrastructure.Database;
 public class CineSlateContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CineSlateContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
---
+++ Server/src/Infrastructure/Database/Configuration/UsersConfiguration.cs
@@ -0,0 +1,36 @@
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.Configuration;
+
+public class UsersConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+
+        builder
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.Role)
+            .IsRequired()
+            .HasMaxLength(20);
+
+        builder.Property(u => u.Password)
+            .IsRequired()
+            .HasMaxLength(200);
+    }
+}
