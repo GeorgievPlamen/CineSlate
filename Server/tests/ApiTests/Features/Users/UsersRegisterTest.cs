5b2b19b8b486ad71f08686e69d03a6238c0ddcd4
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-11-15 16:19:57 +0200

    Setup Integration tests

--- Server/tests/ApiTests/Features/Users/UsersRegisterTest.cs
+++ Server/tests/ApiTests/Features/Users/UsersRegisterTest.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Net.Http.Json;
 using TestUtilities;
 
 namespace ApiTests.Features.Users;
@@ -7,12 +9,20 @@ public class UsersRegisterTest(ApiFactory api) : IClassFixture<ApiFactory>
     private readonly HttpClient _httpClient = api.CreateClient();
 
     [Fact]
-    public void TestName()
+    public async Task Register_ShouldReturnOk201_WhenValid()
     {
-        // Arrange
-    
+        var request = new
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            Password = "Password123!"
+        };
+
         // Act
-    
+        var response = await _httpClient.PostAsJsonAsync("/api/users/register", request);
+
         // Assert
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
     }
 }
--- Server/tests/TestUtilities/ApiFactory.cs
+++ Server/tests/TestUtilities/ApiFactory.cs
@@ -1,9 +1,50 @@
 using Api.Common;
+using Infrastructure.Database;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+using Xunit;
 
 namespace TestUtilities;
 
-public class ApiFactory : WebApplicationFactory<IApiMarker>
+public class ApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
 {
-    
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("testDB")
+        .WithUsername("postgres")
+        .WithPassword("secretpassword")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services.FirstOrDefault(
+                s => s.ServiceType == typeof(DbContextOptions<CineSlateContext>));
+
+            if (descriptor is not null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<CineSlateContext>(options =>
+                options.UseNpgsql(_dbContainer.GetConnectionString()));
+
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<CineSlateContext>();
+            dbContext.Database.Migrate();
+        });
+    }
+
+    public Task InitializeAsync()
+    {
+        return _dbContainer.StartAsync();
+    }
+
+    public new Task DisposeAsync()
+    {
+        return _dbContainer.StopAsync();
+    }
 }
