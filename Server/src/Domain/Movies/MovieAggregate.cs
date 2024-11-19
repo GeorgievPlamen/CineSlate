d4e33e46863d49ad41c31f506718e25a1b292f27
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-11-19 20:24:16 +0200

    Clean up

--- Server/src/Domain/Movies/MovieAggregate.cs
+++ Server/src/Domain/Movies/MovieAggregate.cs
@@ -6,14 +6,14 @@ namespace Domain.Movies;
 public class MovieAggregate : AggregateRoot<MovieId>
 {
     private MovieAggregate(MovieId id) : base(id) { }
+    private List<Genre> _genres = null!;
+    private readonly List<Rating> _ratings = [];
 
     public string Title { get; private set; } = null!;
     public string Description { get; private set; } = null!;
     public DateOnly ReleaseDate { get; private set; }
     public string PosterPath { get; private set; } = null!;
-    private List<Genre> _genres { get; set; } = null!;
     public IReadOnlyList<Genre> Genres => [.. _genres];
-    private List<Rating> _ratings { get; set; } = null!;
     public IReadOnlyCollection<Rating> Ratings => [.. _ratings];
     public double Rating => _ratings.Average(r => r.Value);
 
