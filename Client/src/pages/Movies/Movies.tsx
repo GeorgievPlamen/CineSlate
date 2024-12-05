45c6a54681327aaddbf34013dba83946a2f49cec
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-12-05 08:51:22 +0200

    todos

--- Client/src/pages/Movies/Movies.tsx
+++ Client/src/pages/Movies/Movies.tsx
@@ -5,7 +5,9 @@ import { Movie } from './models/movieType';
 
 function Movies() {
   const movies = useLoaderData() as Paged<Movie>;
-  console.log(movies.values[0]);
+  // TODO add load more button
+  // TODO add suspense and loading fallback
+  // TODO link from movie to movie details
 
   return (
     <article className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:px-40">
