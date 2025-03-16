63c2ea75162ea5fb862e997644f79da020e3a046
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-03-16 21:51:22 +0200

    clean up

--- Server/src/Application/Common/Paged.cs
+++ Server/src/Application/Common/Paged.cs
@@ -1,6 +1,11 @@
 namespace Application.Common;
 
-public record Paged<T>(List<T> Values, int CurrentPage = 1, bool HasNextPage = false, bool HasPreviousPage = false, int TotalCount = 0);
+public record Paged<T>(
+    List<T> Values,
+    int CurrentPage = 1,
+    bool HasNextPage = false,
+    bool HasPreviousPage = false,
+    int TotalCount = 0);
 
 public static class Paged
 {
