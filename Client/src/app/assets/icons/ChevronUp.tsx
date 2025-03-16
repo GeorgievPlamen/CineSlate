f023cfba5869ff66708168660c8baf6134ba96c9
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-03-16 12:42:17 +0200

    Upgraded to react 19 and fixed paging bug

--- Client/src/app/assets/icons/ChevronUp.tsx
+++ Client/src/app/assets/icons/ChevronUp.tsx
@@ -4,18 +4,18 @@ export default function ChevronUp() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="size-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="m4.5 18.75 7.5-7.5 7.5 7.5"
       />
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="m4.5 12.75 7.5-7.5 7.5 7.5"
       />
     </svg>
