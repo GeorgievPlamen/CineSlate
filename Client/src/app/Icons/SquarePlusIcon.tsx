d16bf2904f72e958a5f2012441a28f68589b68a2
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-03-27 08:45:58 +0200

    Added plus icon and added button to movie card

---
+++ Client/src/app/Icons/SquarePlusIcon.tsx
@@ -0,0 +1,19 @@
+export default function SquarePlusIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="36"
+      height="36"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <rect width="18" height="18" x="3" y="3" rx="2" />
+      <path d="M8 12h8" />
+      <path d="M12 8v8" />
+    </svg>
+  );
+}
