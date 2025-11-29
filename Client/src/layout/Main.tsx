5868bf487e43d4ee0eb6896519200c2774e17d41
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-11-29 15:57:07 +0200

    fix tailwind

--- Client/src/layout/Main.tsx
+++ Client/src/layout/Main.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 function Main() {
   return (
-    <main className="mt-[69px] flex-grow">
+    <main className="mt-[69px] grow">
       <Outlet />
     </main>
   );
