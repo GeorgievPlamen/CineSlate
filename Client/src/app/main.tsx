16fbce2e67ae86bab99735b72a2868bf2b08a909
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-11-29 09:46:25 +0200

    Add react query and provider

--- Client/src/app/main.tsx
+++ Client/src/app/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
