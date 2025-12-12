938357418c2675712c56e0927cfb1da7211ac060
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-12-12 09:29:55 +0200

    Fixed movie link and add to watchlist button

--- Client/src/components/Cards/MovieCard.tsx
+++ Client/src/components/Cards/MovieCard.tsx
@@ -1,6 +1,6 @@
 import { IMG_PATH_W500 } from '@/config';
 import SquarePlusIcon from '@/Icons/SquarePlusIcon';
-import { useNavigate } from '@tanstack/react-router';
+import { Link } from '@tanstack/react-router';
 
 interface Props {
   title: string;
@@ -17,18 +17,16 @@ export default function MovieCard({
   id,
   rating,
 }: Props) {
-  const navigate = useNavigate();
   return (
     <article
-      onClick={() =>
-        navigate({
-          to: '/movies/$id',
-          params: { id: `${id}` },
-        })
-      }
-      className="mx-auto flex w-60 justify-between flex-col rounded-lg border border-grey bg-background shadow shadow-dark hover:border-primary active:border-opacity-80"
+      className="mx-auto relative flex w-60 flex-col rounded-lg border border-grey bg-background shadow shadow-dark hover:border-primary active:border-opacity-80"
       id={`${id}`}
     >
+      <Link
+        to="/movies/$id"
+        className="absolute inset-0 z-0"
+        params={{ id: `${id}` }}
+      />
       <img
         className="mb-2 rounded-t-lg"
         src={IMG_PATH_W500 + posterPath}
@@ -36,13 +34,16 @@ export default function MovieCard({
       />
       <div className="mx-2 mb-1 flex justify-between font-primary text-sm">
         <p>⭐{rating}</p>
-        <p>{releaseDate.toString()}</p>
+        <p className="text-muted-foreground">{releaseDate.toString()}</p>
       </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between h-full">
         <p className="mx-2 flex h-full items-center font-heading text-lg">
           {title}
         </p>
-        <button className="mr-1 text-primary hover:text-opacity-80 active:text-opacity-50">
+        <button
+          onClick={() => console.log('add to watchlist')}
+          className="mr-1 text-primary hover:text-opacity-80 active:text-opacity-50 z-10"
+        >
           <SquarePlusIcon />
         </button>
       </div>
