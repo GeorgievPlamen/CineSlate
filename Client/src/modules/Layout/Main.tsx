2ad9348b3d206f38b2a23d75a479b3909e9a1e02
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2026-03-29 14:33:39 +0300

    Restyled footer and scrollbar

--- Client/src/modules/Layout/Main.tsx
+++ Client/src/modules/Layout/Main.tsx
@@ -2,7 +2,7 @@ import { Outlet } from '@tanstack/react-router';
 
 function Main() {
   return (
-    <main className="grow bg-background z-0">
+    <main className="grow overflow-y-auto scrollbar overflow-x-hidden">
       <Outlet />
     </main>
   );
